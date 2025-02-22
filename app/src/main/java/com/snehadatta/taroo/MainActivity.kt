5330e50aa0e4ae628fdd80a3203b2f5f664f62b4
Sneha Datta <snehadatta431@gmail.com>
2025-02-22 08:28:41 +0530

    Modify UI

--- app/src/main/java/com/snehadatta/taroo/MainActivity.kt
+++ app/src/main/java/com/snehadatta/taroo/MainActivity.kt
@@ -4,43 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.font.FontWeight
 import com.snehadatta.taroo.ui.presentation.ChooseDeckScreen
 import com.snehadatta.taroo.ui.theme.TarooTheme
+import com.snehadatta.taroo.ui.theme.orange
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TarooTheme {
-                Scaffold(modifier = Modifier.fillMaxSize().padding(top = 32.dp)) { innerPadding ->
-                    ChooseDeckScreen(modifier = Modifier.padding(innerPadding), "Your message")
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            colors = TopAppBarDefaults.topAppBarColors(containerColor = orange),
+                            title = {
+                                Text(
+                                    text = "Choose a Deck",
+                                    fontWeight = FontWeight.SemiBold
+                                )
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    ChooseDeckScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    TarooTheme {
-//        Greeting("Android")
-//    }
-//}
--- app/src/main/java/com/snehadatta/taroo/ui/presentation/ChooseDeckScreen.kt
+++ app/src/main/java/com/snehadatta/taroo/ui/presentation/ChooseDeckScreen.kt
@@ -1,30 +1,32 @@
 package com.snehadatta.taroo.ui.presentation
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material3.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,117 +37,121 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.DefaultShadowColor
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
-
+import androidx.compose.ui.unit.sp
 import com.snehadatta.taroo.R
 import com.snehadatta.taroo.ui.theme.TarooTheme
-import com.snehadatta.taroo.ui.theme.brown
-import com.snehadatta.taroo.ui.theme.fontFamily
 import com.snehadatta.taroo.ui.theme.orange
-
 
 @Composable
 fun ChooseDeckScreen(
-    modifier: Modifier = Modifier,
-    values:String ) {
+    modifier: Modifier = Modifier
+) {
     val images = listOf(
         R.drawable.cover1,
         R.drawable.cover2,
         R.drawable.cover3,
         R.drawable.cover4
     )
-    var values by remember { mutableStateOf("") }
-    Column {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2), // 2 columns
-            modifier = Modifier
-                .padding(start = 32.dp,top = 32.dp, end = 32.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            items(images) { imageRes ->
-                DeckImage(imageRes)
-            }
-        }
-        Text(
-            text = "Pick a deck of your choice",
-            modifier = Modifier
-                .padding(top= 8.dp)
-                .align(Alignment.CenterHorizontally),
-            fontWeight = FontWeight.Medium,
-            fontStyle = FontStyle.Italic
+    var selectedIndex by remember { mutableStateOf(-1) }
+    var question by remember { mutableStateOf("") }
 
-        )
-        TextField(
-            value = values,
-            onValueChange = {values = it},
+    Column(
+        modifier = modifier
+            .padding(horizontal = 32.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        Box( // Wrapping in a Box with height constraint
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 16.dp)
-                .padding(horizontal = 44.dp),
+                .padding(top = 32.dp)
+                .aspectRatio(0.6f)
+        ) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                itemsIndexed(images) { index, imageRes ->
+                    DeckImage(
+                        imageRes = imageRes,
+                        isSelected = selectedIndex == index,
+                        onClick = { selectedIndex = if (selectedIndex == index) -1 else index }
+                    )
+                }
+            }
+        }
+
+        OutlinedTextField(
+            value = question,
+            onValueChange = { question = it },
+            modifier = Modifier
+                .fillMaxWidth(),
+            singleLine = false,
             maxLines = 15,
-            placeholder = {
-                Text(text = "Enter message...")
+            label = {
+                Text(text = "Enter your question here")
             },
             trailingIcon = {
-                if (values.isNotEmpty()) {
-                    IconButton(onClick = { values = "" }) {
+                if (question.isNotEmpty()) {
+                    IconButton(onClick = { question = "" }) {
                         Icon(Icons.Filled.Clear, contentDescription = "Clear text")
                     }
                 }
-            }
+            },
         )
 
-        Button(
+        OutlinedButton (
             modifier = Modifier
-                .padding(top = 8.dp)
-                .align(Alignment.CenterHorizontally)
-                .width(100.dp),
-            onClick = { /* Action */ }, colors = ButtonDefaults.buttonColors(orange)) {
-            Text("Shuffle",
-                fontFamily = fontFamily
+                .fillMaxWidth()
+                .height(64.dp)
+                .padding(top = 16.dp)
+                .align(Alignment.CenterHorizontally),
+            onClick = { /* Action */ },
+            colors = ButtonDefaults.buttonColors(orange)
+        ) {
+            Text(
+                text = "Shuffle",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.SemiBold
             )
-
         }
+
+        Spacer(modifier = Modifier.height(32.dp))
     }
 
 }
 
 @Composable
-fun DeckImage(imageRes: Int) {
-    Box(
+fun DeckImage(imageRes: Int, isSelected: Boolean, onClick: () -> Unit) {
+    Image(
+        painter = painterResource(id = imageRes),
+        contentDescription = "Deck Cover",
         modifier = Modifier
-            .aspectRatio(1f)
-            .clip(RoundedCornerShape(12.dp)),
-        Alignment.Center
-    ) {
-        Image(
-            painter = painterResource(id = imageRes),
-            contentDescription = "Deck Cover",
-            modifier = Modifier
-                .height(200.dp)
-                .width(100.dp)
-                .background(color = brown, RoundedCornerShape(8.dp))
-                .shadow(elevation = 12.dp, shape = RoundedCornerShape(12.dp)) // Adds shadow
-                .clip(RoundedCornerShape(12.dp)),
-            contentScale = ContentScale.Fit
-        )
-    }
+            .aspectRatio(0.6f)
+            .padding(8.dp)
+            .shadow(elevation = 8.dp, shape = RoundedCornerShape(12.dp)) // Adds shadow
+            .clip(RoundedCornerShape(12.dp))
+            .clickable { onClick() }
+            .border(
+                width = if (isSelected) 6.dp else 0.dp, // Adds a red border if selected
+                color = if (isSelected) orange else Color.Transparent,
+                shape = RoundedCornerShape(12.dp)
+            ),
+        contentScale = ContentScale.Fit
+    )
 }
-@Preview(showBackground = true)
+
+@PreviewLightDark()
 @Composable
 fun DeckPickerPreview() {
     TarooTheme {
-        ChooseDeckScreen(modifier = Modifier,"Nothing")
+        Scaffold { innerPadding ->
+            ChooseDeckScreen(modifier = Modifier.padding(innerPadding))
+        }
     }
 }
